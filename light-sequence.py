d889b3cd240b9d280d5207e3e1351b5eb8e0fd7a
Alex DeBoni <adeboni@scu.edu>
2015-08-28 07:31:29 -0700

    Create light-sequence.py

---
+++ light-sequence.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python 
+
+import select, json, socket, sys, time
+
+PORT = 50000
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(('', 0))
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+seq = [[0,0,0],[0,0,1],[0,1,0],[0,1,1],[1,0,0],[1,0,1],[1,1,0],[1,1,1]]
+
+i = 0
+while True:
+	s.sendto(json.dumps(seq[i%len(seq)]), ('<broadcast>', PORT))
+	i += 1
+	time.sleep(1) # change this according to volume
