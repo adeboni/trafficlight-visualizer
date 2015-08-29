cbeba6c3284b82fffc4071fab739edec01eabc07
Alex DeBoni <adeboni@scu.edu>
2015-08-29 09:36:46 -0700

    updates

--- light-sequence.py
+++ light-sequence.py
@@ -7,10 +7,10 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(('', 0))
 s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-seq = [[[0,0,0]],[[0,0,1]],[[0,1,0]],[[0,1,1]],[[1,0,0]],[[1,0,1]],[[1,1,0]],[[1,1,1]]]
+seq = [[0,0,0],[0,0,1],[0,1,0],[0,1,1],[1,0,0],[1,0,1],[1,1,0],[1,1,1]]
 
 i = 0
 while True:
-	s.sendto(json.dumps(seq[i%len(seq)]), ('<broadcast>', PORT))
+	s.sendto(json.dumps([seq[i%len(seq)],seq[i%len(seq)]]), ('<broadcast>', PORT))
 	i += 1
 	time.sleep(1) # change this according to volume
