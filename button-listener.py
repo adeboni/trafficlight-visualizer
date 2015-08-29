13065745482742ee724878b5c641f673e35a9d6b
Alex DeBoni <adeboni@scu.edu>
2015-08-29 10:06:00 -0700

    updates

--- button-listener.py
+++ button-listener.py
@@ -8,17 +8,22 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(('', 0))
 s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-#wiringpi.wiringPiSetup()
+wiringpi.wiringPiSetup()
 wiringpi.wiringPiSetupGpio()
-wiringpi.pinMode(23, 0)
-wiringpi.pullUpDnControl(23, 2)
-wiringpi.pinMode(24, 0)
-wiringpi.pullUpDnControl(24, 2)
-wiringpi.pinMode(25, 0)
-wiringpi.pullUpDnControl(25, 2)
-
+wiringpi.pinMode(13, 0)
+wiringpi.pullUpDnControl(13, 2)   
+wiringpi.pinMode(19, 0)
+wiringpi.pullUpDnControl(19, 2) 
+wiringpi.pinMode(26, 0)
+wiringpi.pullUpDnControl(26, 2) 
 
 while True:
-	print wiringpi.digitalRead(23)
-	print wiringpi.digitalRead(24)
-	print wiringpi.digitalRead(25)
+	if wiringpi.digitalRead(23):
+		s.sendto(json.dumps([int(sys.argv[1]), 0]), ('<broadcast>', PORT))
+		sleep(0.05)
+	elif wiringpi.digitalRead(24):
+		s.sendto(json.dumps([int(sys.argv[1]), 1]), ('<broadcast>', PORT))
+		sleep(0.05)
+	elif wiringpi.digitalRead(25):
+		s.sendto(json.dumps([int(sys.argv[1]), 2]), ('<broadcast>', PORT))
+		sleep(0.05)
