01c7d369a904620a182933b34bfd2b80c6ff2004
Alex DeBoni <adeboni@scu.edu>
2015-08-29 12:16:33 -0700

    updates

--- light-sequence.py
+++ light-sequence.py
@@ -24,19 +24,10 @@ if has_alsa:
 
 	def audio_listener(x):
 		global vol
-		total = 1
-		count = 1
-		
 		while True:
 			l,data = inp.read()
 			if l:
-				vol = min(1, math.log(1.0 * audioop.max(data, 2)) / 5)
-				if (vol < 1.1 * total / count and vol > 0.9 * total / count):
-					total += vol
-					count += 1
-				vol -= total / count
-				vol /= 2
-				
+				vol = min(1, math.log(1.0 * audioop.max(data, 2)) / 7 - 0.42)
 			time.sleep(0.001)
 
 	thread.start_new_thread(audio_listener, (0, ))
@@ -45,4 +36,4 @@ while True:
 	s.sendto(json.dumps([seq[i%len(seq)],seq[i%len(seq)]]), ('<broadcast>', PORT))
 	i += 1
 	time.sleep(max(0.0, 1.0 - vol))
-	print str(vol) + ', ' + str(max(0.0, 1.0 - vol))
+	print vol
