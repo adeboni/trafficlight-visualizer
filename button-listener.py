cf193c67db6d23c356ca62242f6305a80f161141
Alex DeBoni <adeboni@scu.edu>
2015-08-29 09:52:28 -0700

    updates

--- button-listener.py
+++ button-listener.py
@@ -17,13 +17,26 @@ wiringpi.pullUpDnControl(24, 2)
 wiringpi.pinMode(25, 0)
 wiringpi.pullUpDnControl(25, 2) 
 
+wiringpi.pinMode(16, 1)
+wiringpi.pinMode(20, 1)
+wiringpi.pinMode(21, 1)
+
+val1 = 0
+val2 = 0
+val3 = 0
 while True:
 	if not wiringpi.digitalRead(23):
 		s.sendto(json.dumps([int(sys.argv[1]), 0]), ('<broadcast>', PORT))
+		val1 ^= 1
+		wiringpi.digitalWrite(16, val1)
 		time.sleep(0.05)
 	elif not wiringpi.digitalRead(24):
 		s.sendto(json.dumps([int(sys.argv[1]), 1]), ('<broadcast>', PORT))
+		val2 ^= 1
+		wiringpi.digitalWrite(20, val2)
 		time.sleep(0.05)
 	elif not wiringpi.digitalRead(25):
 		s.sendto(json.dumps([int(sys.argv[1]), 2]), ('<broadcast>', PORT))
+		val3 ^= 1
+		wiringpi.digitalWrite(21, val3)
 		time.sleep(0.05)
