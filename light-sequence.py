55bac41234fa99932b37841a00ba9d712b9fe30c
Alex DeBoni <adeboni@scu.edu>
2015-08-29 11:14:31 -0700

    updates

--- light-sequence.py
+++ light-sequence.py
@@ -15,13 +15,13 @@ inp.setrate(8000)
 inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
 inp.setperiodsize(160)
 
-vol = 2.0
+vol = 1.0
 i = 0
 while True:
 	time.sleep(0.01)
 	l,data = inp.read()
 	if l:
-		vol = math.min(1.0, math.log(1.0 * audioop.max(data, 2) / 750))
+		vol = min(1.0, math.log(1.0 * audioop.max(data, 2) / 750))
 	#s.sendto(json.dumps([seq[i%len(seq)],seq[i%len(seq)]]), ('<broadcast>', PORT))
 	i += 1
 	print vol
