1298cee3e66eae72170b0c4b630b29de77260185
Lance Welsh <lpw@yahoo-inc.com>
2015-09-16 16:05:07 -0700

    script to get and build status and correlate to lights also simple button toggle light script

---
+++ lights-build.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import json, socket, sys, datetime, urllib2, time
+
+light_index = int(sys.argv[1])
+
+MailProd = "http://api.screwdriver.corp.yahoo.com:4080/badge/8507/prod/icon"
+StormQA = "http://api.screwdriver.corp.yahoo.com:4080/badge/13705/qa/icon"
+SearResultsQA = "http://api.screwdriver.corp.yahoo.com:4080/badge/21717/qa/icon"
+URL = MailProd
+
+LIGHT_PORT = 50000
+light_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+light_socket.bind(('', 0))
+light_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+dimx = 3
+dimy = 2
+lights = [[0 for x in range(dimx)] for y in range(dimy)]
+
+def getLights(buildColor):
+    return {
+        'red': [1, 0, 0],
+        'purple': [0, 1, 0],
+        'blue': [0, 1, 1],
+        'lightgrey': [0, 0, 0],
+    }.get(buildColor, [1, 1, 1])
+
+while True:
+	result = urllib2.urlopen(URL).geturl()
+	buildColor = result.split('/')[-1]
+	print buildColor
+	localLights = getLights(buildColor)
+	lights[light_index] = localLights
+	light_socket.sendto(json.dumps(lights), ('<broadcast>', LIGHT_PORT))
+	print lights
+	time.sleep(10)
---
+++ lights-button.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python 
+
+import select, json, socket, sys, datetime
+
+BUTTON_PORT = 50001
+BUFFER = 128
+button_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+button_socket.bind(('0.0.0.0', BUTTON_PORT))
+button_socket.setblocking(0)
+
+
+LIGHT_PORT = 50000
+light_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+light_socket.bind(('', 0))
+light_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+def get_time():
+	x = str(datetime.datetime.now()).split(' ')[1].split(':')
+	return float(x[0])*3600 + float(x[1])*60 + float(x[2])
+
+
+dimx = 3
+dimy = 2
+lights = [[0 for x in range(dimx)] for y in range(dimy)]
+
+def toggle(x, y):
+	lights[x][y] = not lights[x][y];
+
+light_socket.sendto(json.dumps(lights), ('<broadcast>', LIGHT_PORT))
+print lights
+
+slbp = get_time()		
+while True:
+	result = select.select([button_socket],[],[])
+	light_change = json.loads(result[0][0].recv(BUFFER))
+	
+	curr_time = get_time()
+	if false and curr_time - slbp > 123:
+		# reset
+		time.sleep(1)
+		lights = [[0 for x in range(dimx)] for y in range(dimy)]
+	else:
+		toggle(light_change[0], light_change[1])
+	
+	light_socket.sendto(json.dumps(lights), ('<broadcast>', LIGHT_PORT))
+	print lights
+	
+	slbp = get_time()
