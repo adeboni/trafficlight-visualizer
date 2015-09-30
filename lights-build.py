83a6e6bd782a4c8f0e4e3873c864de33218818c4
Lance Welsh <lpw@yahoo-inc.com>
2015-09-30 11:43:31 -0700

    Latch last build state

--- lights-build.py
+++ lights-build.py
@@ -7,7 +7,8 @@ light_index = int(sys.argv[1])
 MailProd = "http://api.screwdriver.corp.yahoo.com:4080/badge/8507/prod/icon"
 StormQA = "http://api.screwdriver.corp.yahoo.com:4080/badge/13705/qa/icon"
 SearResultsQA = "http://api.screwdriver.corp.yahoo.com:4080/badge/21717/qa/icon"
-URL = MailProd
+MailQA = "http://api.screwdriver.corp.yahoo.com:4080/badge/8507/qa/icon"
+URL = MailQA
 
 LIGHT_PORT = 50000
 light_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -21,20 +22,29 @@ light_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 dimx = 3
 dimy = 2
 lights = [[0 for x in range(dimx)] for y in range(dimy)]
+lastFinishedBuild = 'lightgrey';
 
 def getLights(buildColor):
     return {
         'red': [0, 0, 1],
         'purple': [0, 1, 0],
         'blue': [1, 0, 0],
+        'yellow': [1, 0, 1],
         'lightgrey': [0, 0, 0],
     }.get(buildColor, [1, 1, 1])
+
+def orLights( la1, la2 ):
+	return [ la1[x] or la2[x] for x in range(dimx) ]
 
 while True:
 	result = urllib2.urlopen(URL).geturl()
 	buildColor = result.split('/')[-1]
-	print buildColor
+	print buildColor, lastFinishedBuild
 	localLights = getLights(buildColor)
+	if buildColor == 'red' or buildColor == 'blue':
+		lastFinishedBuild = buildColor
+	if lastFinishedBuild == 'red' or lastFinishedBuild == 'blue':
+		localLights = orLights( localLights, getLights(lastFinishedBuild) )
 	lights[light_index] = localLights
 	light_socket.sendto(json.dumps(lights), ('<broadcast>', LIGHT_PORT))
 	print lights
